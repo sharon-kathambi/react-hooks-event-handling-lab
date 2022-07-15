cb928565cb16d90551da789c53698dac29c8f779
Sharon Mwenda <smkathambi@gmail.com>
2022-07-15 11:48:13 +0300

    Done

--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -1,1 +1,15 @@
 // Code Keypad Component Here
+
+function Keypad(){
+    function handleChange(event){
+        console.log("Entering password...");
+    }
+
+    return(
+        <div>
+            <input type="password" name="password" placeholder="Enter Password" onChange={handleChange}></input>
+        </div>
+    )
+}
+
+export default Keypad;
